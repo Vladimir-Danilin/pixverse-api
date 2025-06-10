b1d91a9af6618e8a23080c1eeeeacfe893cd3500
Vladimir-Danilin <ivve.2016@yandex.ru>
2025-06-10 13:59:45 +0600

    update schemas

--- src/schemas/request.py
+++ src/schemas/request.py
@@ -1,20 +1,20 @@
+from fastapi import Query
 from pydantic import BaseModel, Field
 
 
 class TextToVideoRequest(BaseModel):
     app_bundle_id: str = Field(...)
     apphud_user_id: str = Field(...)
-    prompt: str = Field(..., min_length=1, max_length=2048, description="Промпт который передаётся модели для генерации изображения")
+    prompt: str = Field(Query(description="Промпт который передаётся модели для генерации видео"), min_length=1, max_length=2048)
 
 
 class ImageToVideoRequest(BaseModel):
     app_bundle_id: str = Field(...)
     apphud_user_id: str = Field(...)
-    prompt: str = Field(..., min_length=1, max_length=2048, description="Промпт который передаётся модели для генерации изображения")
-    image_data: bytes = Field(..., description="Изображение, на основе которого будет сгенерировано видео"),
+    prompt: str = Field(Query(description="Промпт который передаётся модели для генерации видео"), min_length=1, max_length=2048)
 
 
 class GenerationStatusRequest(BaseModel):
     app_bundle_id: str = Field(...)
     apphud_user_id: str = Field(...)
-    video_id: str = Field(..., description="ID видео, полученное при генерации")
+    video_id: str = Field(..., description="ID видео, полученное при запросе на его генерации")
