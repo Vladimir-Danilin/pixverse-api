ecd01c386d501cc6294a9b268903d9a1c7d9ea08
Vladimir-Danilin <ivve.2016@yandex.ru>
2025-06-11 17:51:38 +0600

    update routers

--- src/interfaces/api/routes.py
+++ src/interfaces/api/routes.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter, HTTPException, Request, Depends, UploadFile, File
+import uuid
+
+from fastapi import APIRouter, Request, Depends, UploadFile, File
+
+from src.domain.models import User
 from src.domain.services import VideoGenerationService
+from src.infra.db.repository import UsersRepository
 from src.schemas.request import TextToVideoRequest, ImageToVideoRequest, GenerationStatusRequest
 from src.schemas.response import GenerationResponse, StatusResponse
 
@@ -11,11 +16,17 @@ def get_video_generation_service(request: Request) -> VideoGenerationService:
     return request.app.state.video_generation_service
 
 
+def get_users_repository(request: Request) -> UsersRepository:
+    return request.app.state.users_repository
+
+
 @router.post("/text2video", response_model=GenerationResponse)
 async def text_to_video(
     request: TextToVideoRequest = Depends(),
-    service: VideoGenerationService = Depends(get_video_generation_service)
+    service: VideoGenerationService = Depends(get_video_generation_service),
+    repository: UsersRepository = Depends(get_users_repository)
 ):
+    await repository.save_user(User(app_bundle_id=request.app_bundle_id, apphud_user_id=request.apphud_user_id))
     result = await service.generate_from_text(request)
     return GenerationResponse(**result)
 
@@ -23,23 +34,21 @@ async def text_to_video(
 @router.post("/image2video", response_model=GenerationResponse)
 async def image_to_video(
     request: ImageToVideoRequest = Depends(),
-    image: UploadFile = File(..., description="Изображения для генерации видео, не должно превышать 20MB и разрешение 4000х4000"),
-    service: VideoGenerationService = Depends(get_video_generation_service)
+    image: UploadFile = File(..., description="Изображения для генерации видео, не должно превышать 20MB и разрешение 4000х4000. Поддерживаемые форматы: *.png, *.webp, *.jpeg, *.jpg"),
+    service: VideoGenerationService = Depends(get_video_generation_service),
+    repository: UsersRepository = Depends(get_users_repository)
 ):
-    try:
-        result = await service.generate_from_image(request.model_dump(), image)
-        return GenerationResponse(**result)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    await repository.save_user(User(app_bundle_id=request.app_bundle_id, apphud_user_id=request.apphud_user_id))
+    result = await service.generate_from_image(request.model_dump(), image)
+    return GenerationResponse(**result)
 
 
 @router.get("/get_status_generate/{video_id}", response_model=StatusResponse)
 async def get_status(
     request: GenerationStatusRequest = Depends(),
-    service: VideoGenerationService = Depends(get_video_generation_service)
+    service: VideoGenerationService = Depends(get_video_generation_service),
+    repository: UsersRepository = Depends(get_users_repository)
 ):
-    try:
-        result = await service.get_status(request)
-        return StatusResponse(**result)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    await repository.save_user(User(app_bundle_id=request.app_bundle_id, apphud_user_id=request.apphud_user_id))
+    result = await service.get_status(request)
+    return StatusResponse(**result)
