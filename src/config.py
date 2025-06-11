08a8ec89fdd610ab69c2e2a173c1fa9b516ec49f
Vladimir-Danilin <ivve.2016@yandex.ru>
2025-06-11 17:52:41 +0600

    change DATABASE_URL

--- src/config.py
+++ src/config.py
@@ -6,4 +6,11 @@ load_dotenv()
 PIXVERSE_API_KEY = os.getenv("PIXVERSE_API_KEY")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
